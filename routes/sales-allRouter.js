821d063364345ffa20f9ed6cc610ad426f939af9
Jonah Greening <116300133+JonahJG@users.noreply.github.com>
2023-07-24 13:23:49 -0230

    Buggy. Moving on for now.

---
+++ routes/sales-allRouter.js
@@ -0,0 +1,47 @@
+
+const express = require('express');
+const router = express.Router();
+const { getRecentSales, getItemsByIds } = require('../services/dataService');
+
+router.get('/', async (req, res) => {
+  try {
+    const limit = 10;
+
+    // Fetch recent sales using the dataService function
+    const sales = await getRecentSales(limit);
+
+    const itemIds = sales.map((sale) => sale.item_id);
+  
+
+    // Fetch items using the dataService function
+    const items = await getItemsByIds(itemIds);
+
+    console.log('Items:', items);
+    console.log('Sales:', sales);
+
+    // Mapping sales with their corresponding items
+    const salesWithItems = sales.map((sale) => {
+      const { item_id, sell_price, sell_time } = sale;
+      const item = items.find((item) => item.item_id === item_id);
+      return {
+        item_id,
+        sell_price,
+        sell_time,
+        item_name: item ? item.item_name : '',
+        item_description: item ? item.item_description : '',
+        item_price: item ? item.item_price : '',
+        amt_sold: sale.amt_sold,
+        total_price: sale.total_price
+      };
+    });
+
+    console.log('Sales With Items:', salesWithItems); 
+
+    res.render('sales-all', { sales: salesWithItems });
+  } catch (error) {
+    console.error('Error fetching sales data:', error);
+    res.status(500).send('Error fetching sales data. Please try again later.');
+  }
+});
+
+module.exports = router;
---
+++ services/dataService.js
@@ -0,0 +1,47 @@
+// dataService.js
+const pool = require('../services/rsge_db');
+
+async function getRecentSales(limit) {
+  const client = await pool.connect();
+  const salesQuery = `
+    SELECT DISTINCT ON (item_id) *
+    FROM sales
+    ORDER BY item_id, sell_time DESC
+    LIMIT $1
+  `;
+  const salesResult = await client.query(salesQuery, [limit]);
+  client.release();
+  return salesResult.rows;
+}
+
+async function getRecentPurchases(limit) {
+  const client = await pool.connect();
+  const purchasesQuery = `
+    SELECT DISTINCT ON (item_id) *
+    FROM purchases
+    ORDER BY item_id, buy_time DESC
+    LIMIT $1
+  `;
+  const purchasesResult = await client.query(purchasesQuery, [limit]);
+  client.release();
+  return purchasesResult.rows;
+}
+
+async function getItemsByIds(itemIds) {
+  const client = await pool.connect();
+  const itemsQuery = `
+    SELECT *
+    FROM items
+    WHERE item_id = ANY($1)
+  `;
+  const itemsResult = await client.query(itemsQuery, [itemIds]);
+  client.release();
+  return itemsResult.rows;
+ 
+}
+
+module.exports = {
+  getRecentSales,
+  getRecentPurchases,
+  getItemsByIds,
+};
